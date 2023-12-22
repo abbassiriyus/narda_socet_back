abd4f7f40342618fd7d30ba58d5561e9e9d88eae
abbas <86982819+abbassiriyus@users.noreply.github.com>
2023-12-22 11:54:35 +0500

    das
    asd

---
+++ front_html/main.js
@@ -0,0 +1,446 @@
+
+var position=[{
+    id:1,
+    position:1,
+    name:'oq'
+},
+{
+    id:2,
+    position:1,
+    name:'oq'
+},{
+    id:3,
+    position:1,
+    name:'oq'
+},{
+    id:4,
+    position:1,
+    name:'oq'
+},
+{
+    id:5,
+    position:1,
+    name:'oq'
+},{
+    id:6,
+    position:1,
+    name:'oq'
+},{
+    id:7,
+    position:1,
+    name:'oq'
+},{
+    id:8,
+    position:1,
+    name:'oq'
+},{
+    id:9,
+    position:1,
+    name:'oq'
+},
+{
+    id:10,
+    position:1,
+    name:'oq'
+},{
+    id:11,
+    position:1,
+    name:'oq'
+},{
+    id:12,
+    position:1,
+    name:'oq'
+},
+{
+    id:13,
+    position:1,
+    name:'oq'
+},{
+    id:14,
+    position:1,
+    name:'oq'
+},{
+    id:15,
+    position:1,
+    name:'oq'
+},{
+    id:1,
+    position:13,
+    name:'qora'
+},
+{
+    id:2,
+    position:13,
+    name:'qora'
+},{
+    id:3,
+    position:13,
+    name:'qora'
+},{
+    id:4,
+    position:13,
+    name:'qora'
+},
+{
+    id:5,
+    position:13,
+    name:'qora'
+},{
+    id:6,
+    position:13,
+    name:'qora'
+},{
+    position:13,
+    name:'qora'
+},{
+    id:8,
+    position:13,
+    name:'qora'
+},{
+    id:9,
+    position:13,
+    name:'qora'
+},
+{
+    id:10,
+    position:13,
+    id:7,
+    name:'qora'
+},{
+    id:11,
+    position:13,
+    name:'qora'
+},{
+    id:12,
+    position:13,
+    name:'qora'
+},
+{
+    id:13,
+    position:13,
+    name:'qora'
+},{
+    id:14,
+    position:13,
+    name:'qora'
+},{
+    id:15,
+    position:13,
+    name:'qora'
+}]
+
+
+
+function positiom(key,d) {
+    switch (key) {
+        case 1:
+            if(d=="oq"){
+                document.querySelectorAll('.circle')[12].innerHTML+=`<div class="img">
+                    <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                </div>`    
+                }else{
+                  document.querySelectorAll('.circle')[12].innerHTML+=`<div class="img">
+             <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+         </div>`       
+                }
+        break;
+        case 2:
+            if(d=="oq"){
+            document.querySelectorAll('.circle')[13].innerHTML+=`<div class="img">
+                <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+            </div>`    
+            }else{
+              document.querySelectorAll('.circle')[13].innerHTML+=`<div class="img">
+         <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+     </div>`       
+            }
+       
+        break;
+        case 3:
+          if(d=="oq"){
+                document.querySelectorAll('.circle')[14].innerHTML+=`<div class="img">
+                    <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                </div>`    
+                }else{
+                  document.querySelectorAll('.circle')[14].innerHTML+=`<div class="img">
+             <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+         </div>`       
+                }
+        break;
+        case 4:
+            if(d=="oq"){
+                document.querySelectorAll('.circle')[15].innerHTML+=`<div class="img">
+                    <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                </div>`    
+                }else{
+                  document.querySelectorAll('.circle')[15].innerHTML+=`<div class="img">
+             <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+         </div>`       
+                }
+        break;
+            case 5:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[16].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[16].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 6:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[17].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[17].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 7:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[18].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[18].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 8:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[19].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[19].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 9:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[20].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-20.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[20].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 10:
+                if(d=="oq"){
+                    document.querySelectorAll[21]('.circle').innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll[21]('.circle').innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 11:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[22].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[22].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 12:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[23].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[23].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 13:
+            if(d=="oq"){
+                document.querySelectorAll('.circle')[11].innerHTML+=`<div class="img">
+                    <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                </div>`    
+                }else{
+                  document.querySelectorAll('.circle')[11].innerHTML+=`<div class="img">
+             <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+         </div>`       
+                }
+        break;
+        case 14:
+            if(d=="oq"){
+            document.querySelectorAl('.circle')[10].innerHTML+=`<div class="img">
+                <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+            </div>`    
+            }else{
+              document.querySelectorAll('.circle')[10].innerHTML+=`<div class="img">
+         <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+     </div>`       
+            }
+       
+        break;
+        case 15:
+          if(d=="oq"){
+                document.querySelectorAll('.circle')[9].innerHTML+=`<div class="img">
+                    <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                </div>`    
+                }else{
+                  document.querySelectorAll('.circle')[9].innerHTML+=`<div class="img">
+             <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+         </div>`       
+                }
+        break;
+        case 16:
+            if(d=="oq"){
+                document.querySelectorAll('.circle')[8].innerHTML+=`<div class="img">
+                    <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                </div>`    
+                }else{
+                  document.querySelectorAll('.circle')[8].innerHTML+=`<div class="img">
+             <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+         </div>`       
+                }
+        break;
+            case 17:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[7].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[7].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 18:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[6].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[6].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 19:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[5].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[5].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 20:
+                if(d=="oq"){
+                    document.querySelectorAll[4]('.circle').innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[4].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 21:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[3].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-20.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[3].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 22:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[2].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[2].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 23:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[1].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[1].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            case 24:
+                if(d=="oq"){
+                    document.querySelectorAll('.circle')[0].innerHTML+=`<div class="img">
+                        <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+                    </div>`    
+                    }else{
+                      document.querySelectorAll('.circle')[0].innerHTML+=`<div class="img">
+                 <img src="./img/photo_2023-12-21_16-55-17.jpg" alt="">
+             </div>`       
+                    }
+            break;
+            default:
+            break;
+    }
+}
+
+var image
+for (let i = 0; i < position.length; i++) {
+//   positiom(position[i].position,position[i].name)
+if(position[i].name=="oq"){
+ image="https://media.licdn.com/dms/image/D5622AQGaUWuO9DosMA/feedshare-shrink_2048_1536/0/1681815184413?e=2147483647&v=beta&t=OXwDfLjO52tJOYWYpn7CGJtd9UBwSZuUg6JsLsjeekA"
+}else{
+image="https://www.usainteanne.ca/cache/images/Georges_M._Liboy_2017_v2_e182e.jpg"
+}
+
+if(position[i].position<12){
+   console.log(12-position[i].position,image); 
+document.querySelectorAll('.circle')[12-position[i].position].innerHTML+=`<div ondrop="drop(event)" ondragover="allowDrop(event)" class="img">
+<img draggable="false" ondragstart="dragStart(event,${i})" src='${image}' class="dragtarget" id='d${i}' alt="uytgguhgh">
+</div>`
+}else{
+document.querySelectorAll('.circle')[position[i].position-1].innerHTML+=`<div ondrop="drop(event)" ondragover="allowDrop(event)" class="img">
+<img draggable="false" ondragstart="dragStart(event,${i})" src='${image}' class="dragtarget" id='d${i}' alt="hjhjhjhj">
+</div>`
+}}
+
+  function dragStart(event) {
+    event.dataTransfer.setData("Text", event.target.src);
+  }
+  
+  
+  function allowDrop(event) {
+    event.preventDefault();
+  }
+  
+  function drop(event) {
+    event.preventDefault();
+    const data = event.dataTransfer.getData("Text");
+    // var newChild = document.createElement("div");
+    var new_image = document.createElement('img')
+    new_image.style="width:30px"
+    new_image.draggable='false'
+    new_image.src = data
+    // newChild.appendChild(new_image)
+    event.target.appendChild(new_image);
+  }
