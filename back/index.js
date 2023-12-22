1bdf4f4a05a4051992afe42995bc1bca51da7467
abbas <86982819+abbassiriyus@users.noreply.github.com>
2023-12-22 12:02:18 +0500

    asd
    das

--- back/index.js
+++ back/index.js
@@ -3,9 +3,19 @@ const { createServer } = require('node:http');
 const { join } = require('node:path');
 const app = express();
 const server = createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, 'index.html'));
+  });
+
+
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
   });
 
 server.listen(5000, () => {
