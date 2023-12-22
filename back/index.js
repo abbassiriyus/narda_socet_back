01bf1ed789c1241b7325d2a4e1b35e3c444c1459
abbas <86982819+abbassiriyus@users.noreply.github.com>
2023-12-22 11:56:43 +0500

    asd
    asd

--- back/index.js
+++ back/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { createServer } = require('node:http');
-
+const { join } = require('node:path');
 const app = express();
 const server = createServer(app);
 
-app.get('/', (req, res) => {-
-  res.send('<h1>Hello world</h1>');
-});
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+  });
 
 server.listen(5000, () => {
   console.log('server running at http://localhost:5000');
