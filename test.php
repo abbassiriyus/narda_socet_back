bf1ce0527344e63c1285d1883c20718b52a41680
abbas <86982819+abbassiriyus@users.noreply.github.com>
2024-01-04 11:08:57 +0500

    ewq
    ew

---
+++ test.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Example</title>
+</head>
+<body>
+    <h1>PHP Example</h1>
+    
+    <?php
+    // PHP kodlaringizni shu yerdan boshlang
+    $name = "John Doe";
+    echo "Hello, $name!";
+    ?>
+    
+    <p>This is an example of using PHP in HTML.</p>
+</body>
+</html>
